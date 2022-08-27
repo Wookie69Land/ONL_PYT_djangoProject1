d457730996cb3c6f93cef003873e9d80fab497d2
Wookie69Land <lukaskraj@gmail.com>
2022-08-27 11:23:34 +0200

    Remove secret settings

--- day_one/views.py
+++ day_one/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def show_number(request, number):
+    answer = f"""
+             <html>
+                <body>
+                    <p>The answer is {number}!</p>
+                </body>
+             </html>
+             """
+
+    return HttpResponse(answer)
--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5geu3hf1y&la!g@%la)69f047x7^u(5ot!k9c_9ouuvv+jqb@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
